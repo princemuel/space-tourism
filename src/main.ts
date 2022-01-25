a36663225e35120fdb501cbfa03f4f622150022e
pHoeniX-svg <vansomecsam@gmail.com>
2022-01-25 16:49:57 +0100

    first commit on project setup

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+console.log('Hello! This is a Typescript only project');
+
+// const article = document.querySelector('#electric-cars');
+// // The following would also work:
+// // const article = document.getElementById("electric-cars")
+
+// article.dataset.columns // "3"
+// article.dataset.indexNumber // "12314"
+// article.dataset.parent // "cars"
