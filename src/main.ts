73589f30e8ae9110fc0a431b0fc7928527d6850b
pHoeniX-svg <vansomecsam@gmail.com>
2022-02-01 03:48:46 +0100

    added tab switching between articles on destination page

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// NAVIGATION
 const nav = document.querySelector('.primary-navigation') as HTMLUListElement;
 const navBtn = document.querySelector(
   '.btn--mobile-toggle'
@@ -23,3 +24,61 @@ navBtn.addEventListener('click', () => {
 // }, [] as string[]);
 
 // console.log(categories);
+
+// TABS
+const tabList = document.querySelector('[role="tablist"]')! as HTMLDivElement;
+const tabs = [
+  ...tabList.querySelectorAll('[role="tab"]'),
+]! as HTMLButtonElement[];
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', changeTabPanel);
+});
+
+let tabFocus = 0;
+tabList.addEventListener('keydown', changeTabFocus);
+
+function changeTabFocus(e: KeyboardEvent) {
+  const keyDownLeft = 37;
+  const keyDownRight = 39;
+
+  if (e.keyCode === keyDownLeft || e.keyCode === keyDownRight) {
+    tabs[tabFocus].setAttribute('tabindex', '-1');
+  }
+
+  if (e.keyCode === keyDownRight) {
+    tabFocus++;
+    if (tabFocus >= tabs.length) tabFocus = 0;
+  }
+
+  if (e.keyCode === keyDownLeft) {
+    tabFocus--;
+    if (tabFocus < 0) tabFocus = tabs.length - 1;
+  }
+
+  tabs[tabFocus].setAttribute('tabindex', '0');
+  tabs[tabFocus].focus();
+}
+
+function changeTabPanel(e: MouseEvent) {
+  const targetTab = e.target as HTMLButtonElement;
+  const targetPanel = targetTab.getAttribute('aria-controls')!;
+  const targetImage = targetTab.getAttribute('data-image')!;
+
+  console.log(targetImage);
+
+  const tabContainer = targetTab.parentElement!;
+  const mainContainer = tabContainer.parentElement!;
+
+  mainContainer.querySelectorAll('[role="tabpanel"]').forEach((panel) => {
+    panel.setAttribute('hidden', 'true');
+  });
+  mainContainer.querySelector(`#${targetPanel}`)!.removeAttribute('hidden');
+
+  mainContainer.querySelectorAll('picture').forEach((picture) => {
+    console.log(picture);
+
+    picture.setAttribute('hidden', 'true');
+  });
+  mainContainer.querySelector(`#${targetImage}`)!.removeAttribute('hidden');
+}
